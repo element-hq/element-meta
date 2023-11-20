fd0836e73f3117f35d47e53ae26e096501107cc0
Johannes Marbach <johannesm@element.io>
2023-11-20 15:20:31 +0100

    1st attempt at mangling YAMLs and (dry) syncing them with github-label-sync

---
+++ .github/actions/sync-labels.ts
@@ -0,0 +1,34 @@
+import fs from "fs";
+import { Command } from "commander";
+import githubLabelSync, { LabelInfo } from "github-label-sync";
+import YAML from "yaml";
+
+const cmd = new Command("sync-labels.ts")
+  .option('--labels <path...>')
+  .parse();
+const opts = cmd.opts();
+console.log(opts)
+const merged = new Map<string, LabelInfo>();
+
+for (const path of opts.labels) {
+  const labels = YAML.parse(fs.readFileSync(path, "utf-8")) as [LabelInfo];
+  for (const label of labels) {
+    if (!label.name) {
+      console.warn(`Ignoring invalid label without a name: ${label}`);
+      continue;
+    }
+    if (merged.has(label.name)) {
+      console.log(`Overriding label ${label.name} from ${path}`);
+    } else {
+      console.log(`Importing label ${label.name} from ${path}`);
+    }
+    merged.set(label.name, label);
+  }
+}
+
+githubLabelSync({
+	accessToken: process.env.GITHUB_TOKEN!,
+	repo: process.env.GITHUB_REPO!,
+	labels: [...merged.values()],
+  dryRun: true, // TODO: Remove this once we know this thing actually works
+}).then(diff => console.log(`Updated labels with diff: ${JSON.stringify(diff)}`));
