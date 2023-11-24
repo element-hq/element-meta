ae67c9db60d726e16e1b2c441b61bc8402df14b5
Johannes Marbach <johannesm@element.io>
2023-11-24 16:42:33 +0100

    Adapt script to support repo slugs

--- .github/actions/sync-labels.ts
+++ .github/actions/sync-labels.ts
@@ -1,59 +1,101 @@
-import fs from "fs";
+import fs, { stat } from "fs";
 import { Command } from "commander";
 import githubLabelSync, { LabelDiff, LabelInfo } from "github-label-sync";
+import github from "octonode";
 import YAML from "yaml";
 
-const accessToken = process.env.GITHUB_TOKEN!;
-const repo = process.env.GITHUB_REPOSITORY!;
-console.log(`Repository: ${repo}`);
+main();
 
-const cmd = new Command("sync-labels.ts")
-  .option('--labels <path...>')
-  .parse();
-const opts = cmd.opts();
-console.log(`Input files: ${opts.labels}`);
-
-const merged = new Map<string, LabelInfo>();
-
-for (const path of opts.labels) {
-  const labels = YAML.parse(fs.readFileSync(path, "utf-8")) as [LabelInfo];
-  if (!labels) {
-    continue;
-  }
-
-  for (const label of labels) {
-    if (!label.name) {
-      console.warn(`Ignoring invalid label without a name: ${label}`);
+async function main() {
+  const accessToken = process.env.GITHUB_TOKEN!;
+  const repo = process.env.GITHUB_REPOSITORY!;
+  console.log(`Repository: ${repo}`);
+  
+  const cmd = new Command("sync-labels.ts")
+    .option('--labels <path...>')
+    .parse();
+  const opts = cmd.opts();
+  console.log(`Input files: ${opts.labels}`);
+  
+  const merged = new Map<string, LabelInfo>();
+  
+  for (const arg of opts.labels) {
+    console.log(`Loading labels from ${arg}`);
+    const labels = fs.existsSync(arg) ? readLabelsFromFile(arg) : await readLabelsFromRepo(arg, accessToken);
+    if (!labels) {
       continue;
     }
-    if (merged.has(label.name)) {
-      console.log(`Overriding label ${label.name} from ${path}`);
-    } else {
-      console.log(`Importing label ${label.name} from ${path}`);
+  
+    for (const label of labels) {
+      if (!label.name) {
+        console.warn(`Ignoring invalid label without a name: ${label}`);
+        continue;
+      }
+      if (merged.has(label.name)) {
+        console.log(`Overriding label ${label.name}`);
+      } else {
+        console.log(`Importing label ${label.name}`);
+      }
+      merged.set(label.name, label);
     }
-    merged.set(label.name, label);
   }
+
+  githubLabelSync({
+    accessToken,
+    repo,
+    labels: [...merged.values()],
+    dryRun: true, // TODO: Remove this once we know this thing actually works
+  }).then((diffs: LabelDiff[]) => {
+    console.log(`Label sync completed ${diffs.length ? "with" : "without"} changes`);
+    for (const diff of diffs) {
+      console.log(`${diff.type} ${diff.name}`);
+      if (diff.expected?.description != diff.actual?.description) {
+        console.log(`  description (actual)   - ${diff.actual?.description}`);
+        console.log(`  description (expected) - ${diff.expected?.description}`);
+      }
+      if (diff.expected?.color != diff.actual?.color) {
+        console.log(`  color (actual)   - ${diff.actual?.color}`);
+        console.log(`  color (expected) - ${diff.expected?.color}`);
+      }
+    }
+  }).catch(error => {
+    console.error(error);
+    throw error;
+  });
+  
 }
 
-githubLabelSync({
-  accessToken,
-  repo,
-  labels: [...merged.values()],
-  dryRun: true, // TODO: Remove this once we know this thing actually works
-}).then((diffs: LabelDiff[]) => {
-  console.log(`Label sync completed ${diffs.length ? "with" : "without"} changes`);
-  for (const diff of diffs) {
-    console.log(`${diff.type} ${diff.name}`);
-    if (diff.expected?.description != diff.actual?.description) {
-      console.log(`  description (actual)   - ${diff.actual?.description}`);
-      console.log(`  description (expected) - ${diff.expected?.description}`);
+function readLabelsFromFile(file: string): LabelInfo[] {
+  const content = fs.readFileSync(file, "utf-8");
+  return YAML.parse(content) as [LabelInfo];
+}
+
+async function readLabelsFromRepo(slug: string, accessToken: string): Promise<LabelInfo[]> {
+  const client = github.client(accessToken);
+
+  const labels: LabelInfo[] = [];
+  const params = { page: 1, per_page: 100 };
+
+  while (true) {
+    const [status, chunk] = await client.getAsync(`/repos/${slug}/labels`, params);
+    if (status !== 200) {
+      throw `Fetching labels failed: ${status}`;
     }
-    if (diff.expected?.color != diff.actual?.color) {
-      console.log(`  color (actual)   - ${diff.actual?.color}`);
-      console.log(`  color (expected) - ${diff.expected?.color}`);
+
+    for (const item of chunk) {
+      labels.push({
+        name: item.name,
+        description: item.description ?? "",
+        color: item.color,
+      })
     }
+
+    if (labels.length !== params.per_page) {
+      break;
+    }
+    
+    params.page += 1;
   }
-}).catch(error => {
-  console.error(error);
-  throw error;
-});
+
+  return labels;
+}
