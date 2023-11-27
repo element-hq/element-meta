9131fbd9af12d7b38279b10c473328a8d6f516a2
Johannes Marbach <johannesm@element.io>
2023-11-27 10:04:19 +0100

    Add delete & wet option and format diff output

--- .github/actions/sync-labels.ts
+++ .github/actions/sync-labels.ts
@@ -1,4 +1,4 @@
-import fs, { stat } from "fs";
+import fs from "fs";
 import { Command } from "commander";
 import githubLabelSync, { LabelDiff, LabelInfo } from "github-label-sync";
 import github from "octonode";
@@ -10,22 +10,30 @@ async function main() {
   const accessToken = process.env.GITHUB_TOKEN!;
   const repo = process.env.GITHUB_REPOSITORY!;
   console.log(`Repository: ${repo}`);
-  
+
   const cmd = new Command("sync-labels.ts")
-    .option('--labels <path...>')
+    .option("--delete", "Removes labels that exist in the repository but are missing from all sources", false)
+    .option("--labels <path...>", "Label source as GitHub repository slug or path to local YAML file. Can be specified multiple times.")
+    .option("--wet", "Write changes, *don't* run in dry mode", false)
     .parse();
   const opts = cmd.opts();
-  console.log(`Input files: ${opts.labels}`);
+  console.log(`Options: ${JSON.stringify(opts)}`);
   
+  const merged = await readAndMergeLabels(opts.labels, accessToken);
+  await syncLabels(merged, repo, accessToken, opts.delete, opts.wet);
+}
+
+async function readAndMergeLabels(srcs: string[], accessToken: string): Promise<Map<string, LabelInfo>> {
   const merged = new Map<string, LabelInfo>();
-  
-  for (const arg of opts.labels) {
-    console.log(`Loading labels from ${arg}`);
-    const labels = fs.existsSync(arg) ? readLabelsFromFile(arg) : await readLabelsFromRepo(arg, accessToken);
+
+  for (const src of srcs) {
+    console.log();
+    console.log(`Loading labels from ${src}`);
+    const labels = fs.existsSync(src) ? readLabelsFromFile(src) : await readLabelsFromRepo(src, accessToken);
     if (!labels) {
       continue;
     }
-  
+
     for (const label of labels) {
       if (!label.name) {
         console.warn(`Ignoring invalid label without a name: ${label}`);
@@ -34,40 +42,18 @@ async function main() {
       if (merged.has(label.name)) {
         console.log(`Overriding label ${label.name}`);
       } else {
-        console.log(`Importing label ${label.name}`);
+        console.log(`Adding label ${label.name}`);
       }
       merged.set(label.name, label);
     }
   }
 
-  githubLabelSync({
-    accessToken,
-    repo,
-    labels: [...merged.values()],
-    dryRun: true, // TODO: Remove this once we know this thing actually works
-  }).then((diffs: LabelDiff[]) => {
-    console.log(`Label sync completed ${diffs.length ? "with" : "without"} changes`);
-    for (const diff of diffs) {
-      console.log(`${diff.type} ${diff.name}`);
-      if (diff.expected?.description != diff.actual?.description) {
-        console.log(`  description (actual)   - ${diff.actual?.description}`);
-        console.log(`  description (expected) - ${diff.expected?.description}`);
-      }
-      if (diff.expected?.color != diff.actual?.color) {
-        console.log(`  color (actual)   - ${diff.actual?.color}`);
-        console.log(`  color (expected) - ${diff.expected?.color}`);
-      }
-    }
-  }).catch(error => {
-    console.error(error);
-    throw error;
-  });
-  
+  return merged;
 }
 
 function readLabelsFromFile(file: string): LabelInfo[] {
   const content = fs.readFileSync(file, "utf-8");
-  return YAML.parse(content) as [LabelInfo];
+  return YAML.parse(content) as LabelInfo[];
 }
 
 async function readLabelsFromRepo(slug: string, accessToken: string): Promise<LabelInfo[]> {
@@ -90,7 +76,7 @@ async function readLabelsFromRepo(slug: string, accessToken: string): Promise<La
       })
     }
 
-    if (labels.length !== params.per_page) {
+    if (chunk.length !== params.per_page) {
       break;
     }
     
@@ -98,4 +84,67 @@ async function readLabelsFromRepo(slug: string, accessToken: string): Promise<La
   }
 
   return labels;
+}
+
+async function syncLabels(labels: Map<string, LabelInfo>, repo: string, accessToken: string, deleteAddedLabels: boolean, writeChanges: boolean) {
+  console.log();
+  console.log("Syncing labels");
+
+  const diffs = await githubLabelSync({
+    accessToken,
+    repo,
+    labels: [...labels.values()],
+    dryRun: !writeChanges,
+    allowAddedLabels: !deleteAddedLabels
+  });
+
+  if (!diffs.length) {
+    console.log(`Label sync completed without changes`);
+    return;
+  }
+
+  logDiffs(diffs, repo);
+}
+
+function logDiffs(diffs: LabelDiff[], repo: string) {
+  const missing = diffs.filter(d => d.type == "missing");
+  if (missing.length > 0) {
+    console.log();
+    console.log(`The following labels are missing in ${repo} but exist in one of the sources. They will be created.`)
+    for (const diff of missing) {
+      console.log(`- name: "${diff.expected?.name}"`);
+    }
+  }
+
+  const changed = diffs.filter(d => d.type == "changed");
+  if (changed.length > 0) {
+    console.log()
+    console.log(`The following labels exist in ${repo} but differ in one of the sources. They will be modified.`)
+    for (const diff of changed) {
+      console.log(`- name: "${diff.actual?.name}"`);
+      if (diff.expected?.description != diff.actual?.description) {
+        console.log(`  description (actual):   ${diff.actual?.description}`);
+        console.log(`  description (expected): ${diff.expected?.description}`);
+      }
+      if (diff.expected?.color != diff.actual?.color) {
+        console.log(`  color (actual):   ${diff.actual?.color}`);
+        console.log(`  color (expected): ${diff.expected?.color}`);
+      }
+    }
+  }
+
+  const added = diffs.filter(d => d.type == "added");
+  if (added.length > 0) {
+    console.log();
+    console.log(`The following labels exist in ${repo} but are missing in all sources. They will be deleted.`)
+    for (const diff of added) {
+      console.log(`- name: "${diff.actual?.name}"`);
+      if (diff.actual?.description) {
+        console.log(`  description: "${diff.actual?.description}"`);
+      }
+      if (diff.actual?.color.length) {
+        console.log(`  color: "${diff.actual?.color}"`);
+      }
+    }
+  }
 }
